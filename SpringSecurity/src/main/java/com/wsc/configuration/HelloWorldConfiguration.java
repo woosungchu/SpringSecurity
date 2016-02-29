347fc7615888071fd3dc84a1db5286bfebe01cc3
woosungchu <woosungchu@gmail.com>
2016-02-29 11:33:43 +0900

    엉망진창이지만 되긴 됨

--- SpringSecurity/src/main/java/com/wsc/configuration/HelloWorldConfiguration.java
+++ SpringSecurity/src/main/java/com/wsc/configuration/HelloWorldConfiguration.java
@@ -4,16 +4,30 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 import org.springframework.web.servlet.view.JstlView;
 
-@Configuration
-@EnableWebMvc
+@Configuration// Add @Configuration, this will let spring know this contains bean definitions.
+@EnableWebMvc// <mvc:annotation-driven/>
 @ComponentScan(basePackages = "com.wsc")
 public class HelloWorldConfiguration extends WebMvcConfigurerAdapter{//extended at Practice2
+
+//Configure ResourceHandlers to serve static resources like CSS/ Javascript etc...
+// <resources mapping="/static/**" location="/static/">
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		registry.addResourceHandler("/static/**").addResourceLocations("/static/");
+	}
+
+	// <mvc:default-servlet-handler>에 해당됨.
+//    @Override
+//    public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
+//        configurer.enable();
+//    }
 
 	@Bean//(name="HelloWorld") // deleted at Practice 2
 	public ViewResolver viewResolver() {
@@ -24,13 +38,5 @@ public class HelloWorldConfiguration extends WebMvcConfigurerAdapter{//extended 
 
 		return viewResolver;
 	}
-
-//Configure ResourceHandlers to serve static resources like CSS/ Javascript etc...
-	@Override
-	public void addResourceHandlers(ResourceHandlerRegistry registry) {
-		registry.addResourceHandler("/static/**").addResourceLocations("/static/");
-	}
-
-
 
 }
